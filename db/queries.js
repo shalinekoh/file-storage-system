2e1f2b02253f3ccaa54a10f7d492eb501c6b63f6
shalinekoh <kohshaline@gmail.com>
2024-10-14 12:02:09 +0800

    Add findUser and addUser queries

--- db/queries.js
+++ db/queries.js
@@ -1,19 +1,38 @@
 const { PrismaClient } = require("@prisma/client");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const bcrypt = require("bcrypt");
 
 const prisma = new PrismaClient();
 
-const findUsers = async () => {
-  const allUsers = await prisma.user.findMany();
-  return allUsers;
+const findUser = async (username) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username: username },
+    });
+    return user;
+  } catch (error) {
+    console.log("Error finding user", error);
+    throw error;
+  }
 };
 
-const createUser = async (username, password) => {
-  return await prisma.user.create({
-    data: {
-      username: username,
-      password: password,
-    },
-  });
+const addUser = async (username, password) => {
+  try {
+    const user = await findUser(username);
+    if (!user) {
+      const hashedPassword = await bcrypt(password, 10);
+      return await prisma.user.create({
+        data: {
+          username: username,
+          password: hashedPassword,
+        },
+      });
+    }
+    console.log("User exist!");
+  } catch (error) {
+    console.log("Error creating user", error);
+    throw error;
+  }
 };
 
-module.exports = { findUsers, createUser };
+module.exports = { findUser, addUser };
