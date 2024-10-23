dcc5eb01b6a1f983d547ef2048acede56866a846
shalinekoh <kohshaline@gmail.com>
2024-10-23 18:14:23 +0800

    Implement new CSS styles for folder and file display

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use("/styles", express.static(path.join(__dirname, "styles")));
 
 app.use(
   expressSession({
